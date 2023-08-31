4249299e8c4d7c2274a33f983433a5548b2638bb
Niloy150042 <mehedislam150042@gmail.com>
2023-08-31 23:34:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// alert("hey!!!");
+let countDate= new Date('September 24, 2023 00:00:00').getTime();
+function CountDown(){
+    let now = new Date().getTime();
+    gap = countDate-now;
+
+    let second=1000;
+    let minute= second*60;
+    let hour = minute *60;
+    let day = hour*24;
+
+    let d= Math.floor(gap/(day));
+    let h= Math.floor((gap %(day))/(hour));
+    let m= Math.floor((gap %(hour))/(minute));
+    let s= Math.floor((gap %(minute))/(second));
+
+
+    document.getElementById('hour').innerText = h;
+    document.getElementById('day').innerText = d;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
